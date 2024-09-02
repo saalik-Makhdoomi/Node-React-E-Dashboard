58810728ce2b256ea610ac4a3bce7831e71d0d93
saalik-Makhdoomi <makhdoomisaalik@gmail.com>
2024-09-02 12:14:01 +0530

    register v-1.0.0

---
+++ front-end/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Nav from './components/Nav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import SignUp from './components/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter> 
+       <Nav />
+          <Routes>
+      <Route path='/' element={<h1>Product Component</h1>} />
+      <Route path='/add' element={<h1>AddProduct Component</h1>} />
+      <Route path='/update' element={<h1>Update Product Component</h1>} />
+      <Route path='/logout' element={<h1>Logout Product Component</h1>} />
+      <Route path='/profile' element={<h1>Profile Component</h1>} />
+      <Route path='/signup' element={<SignUp />} />
+           </Routes>
+      </BrowserRouter>
+      <Footer />
+    
+
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link 
+    } from "react-router-dom";
+
+const Nav=()=>{
+    return(
+        <div>
+            <ul className="nav-ul">
+                <li><Link to="/">Products</Link></li>
+                <li><Link to="/add">Add Products</Link></li>
+                <li><Link to="/update">Update Products</Link></li>
+                <li><Link to="/logout">Logout </Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+                <li><Link to="/signup">SignUp</Link></li>
+
+
+
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
