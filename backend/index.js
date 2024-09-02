78d7dd93dfd5464d7a3b805319f6e7faa7381458
saalik-Makhdoomi <makhdoomisaalik@gmail.com>
2024-09-02 22:45:31 +0530

    MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
-app.get('/', (req,res) => {
-    res.send("app is working")
-});
+const connectDB = async ()=> {
+    mongoose.connect("mongodb://localhost:27017/e-comm");
+    const productSchema = new mongoose.Schema({});
+    const product = mongoose.model("products", productSchema);
+    const data = await product.find();
+    console.warn(data);
 
+}
+
+connectDB();
+
+
+app.get("/",(req,res)=>{
+    res.send("app is working...")
+});
 
 app.listen(5000);
