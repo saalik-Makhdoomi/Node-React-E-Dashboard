fc4d3f03ad670bcce3ed185b7029b8389271f23a
saalik-Makhdoomi <makhdoomisaalik@gmail.com>
2024-09-11 23:34:40 +0530

    logout

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,9 +1,17 @@
 import React, {} from "react";
-import { Link, 
+import { Link, useNavigate
     } from "react-router-dom";
 
 const Nav=()=>{
     const auth=localStorage.getItem('user');
+    const navigate = useNavigate();
+
+
+    const logout =()=>{
+       localStorage.clear();
+       navigate('/signup')
+    }
+
 
      return(
         <div>
@@ -12,7 +20,7 @@ const Nav=()=>{
                 <li><Link to="/add">Add Products</Link></li>
                 <li><Link to="/update">Update Products</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
-                <li> { auth ? <Link to="/logout">Logout </Link> :
+                <li> { auth ? <Link onClick={logout} to="/signup">Logout </Link> :
                 <Link to="/signup">SignUp</Link>}</li>
 
 
