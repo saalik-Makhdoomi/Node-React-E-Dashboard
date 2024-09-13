ecc13cf6de1fda640644db3f7a15d5032676cac9
saalik-Makhdoomi <makhdoomisaalik@gmail.com>
2024-09-13 23:58:07 +0530

    login-API

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
 // import { handleLogin } from "../../../backend/controllers/userController";
 
 const Login = () => {
+
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const navigate = useNavigate();
+
+
+  useEffect(()=>{
+const auth = localStorage.getItem('user');
+if(auth)
+{
+  navigate("/")
+}
+  },)
+
+
   const loginHandle= async ()=>{
     console.warn("email, password", email, password)
+    let result = await fetch('http://localhost:5000/user/login',{
+      method:'post',
+      body:JSON.stringify({email, password}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    result = await result.json();
+    console.log(result);
+    if(!result.name){
+      localStorage.setItem("user",JSON.stringify(result));
+      navigate('/')
+
+    }else{
+      alert("Please Enter Correct Details")
+    }
+    
   }
 
   return (
--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -20,12 +20,17 @@ const Nav=()=>{
                 <li><Link to="/add">Add Products</Link></li>
                 <li><Link to="/update">Update Products</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
-                <li> { auth ? <Link onClick={logout} to="/signup">Logout </Link> :
+                {/* <li> { auth ? <Link onClick={logout} to="/signup">Logout </Link> :
                 <Link to="/signup">SignUp</Link>}</li>
-                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/login">Login</Link></li> */}
 
-
-
+                {
+                    auth ?<li> <Link onClick={logout} to="/signup">Logout </Link></li>
+                    :<>
+                     <li><Link to="/signup">SignUp</Link></li>
+                     <li><Link to="/login">Login</Link></li>
+                    </>
+                }
 
             </ul>
         </div>
