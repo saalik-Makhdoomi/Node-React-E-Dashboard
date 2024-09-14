4ce9797f98ed3be8bca41607c9ee6ff195a40b46
saalik-Makhdoomi <makhdoomisaalik@gmail.com>
2024-09-15 00:15:42 +0530

    files-update

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,9 +1,9 @@
 import React, {} from "react";
 import { Link, useNavigate
     } from "react-router-dom";
-
+import "./Nav.css";
 const Nav=()=>{
-    const auth=localStorage.getItem('user');
+    const auth = localStorage.getItem('user');
     const navigate = useNavigate();
 
 
@@ -15,24 +15,23 @@ const Nav=()=>{
 
      return(
         <div>
-            <ul className="nav-ul">
+            {auth ? < ul className="nav-ul">
                 <li><Link to="/">Products</Link></li>
                 <li><Link to="/add">Add Products</Link></li>
                 <li><Link to="/update">Update Products</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
+                <li> <Link onClick={logout} to="/signup">Logout </Link></li>
                 {/* <li> { auth ? <Link onClick={logout} to="/signup">Logout </Link> :
                 <Link to="/signup">SignUp</Link>}</li>
                 <li><Link to="/login">Login</Link></li> */}
 
-                {
-                    auth ?<li> <Link onClick={logout} to="/signup">Logout </Link></li>
-                    :<>
-                     <li><Link to="/signup">SignUp</Link></li>
-                     <li><Link to="/login">Login</Link></li>
-                    </>
-                }
-
             </ul>
+                :
+            <ul className="nav-ul">
+                <li><Link to="/signup">SignUp</Link></li>
+                <li><Link to="/login">Login</Link></li>
+            </ul>
+            }
         </div>
     )
 }
--- front-end/src/components/SignUp.js
+++ front-end/src/components/SignUp.js
@@ -1,5 +1,6 @@
 import React, { useState,useEffect } from "react";
-import {useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom';
+import "./Signup.css";
 
 const SignUp = () => {
   const [name, setName] = useState("");
